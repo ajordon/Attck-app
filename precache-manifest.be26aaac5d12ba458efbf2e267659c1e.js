28abcfdcc302fc253f1c7b19a290bd39c637002c
root <root@beastMode.localdomain>
2019-09-11 16:53:43 -0400

    Updates

---
+++ precache-manifest.be26aaac5d12ba458efbf2e267659c1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b191b1716e52bad1e08566906530cbdf",
+    "url": "/Attck-app/index.html"
+  },
+  {
+    "revision": "5da434924670aa8bf02c",
+    "url": "/Attck-app/static/css/main.266bad16.chunk.css"
+  },
+  {
+    "revision": "f0a7159c1878363b3799",
+    "url": "/Attck-app/static/js/2.632e02ad.chunk.js"
+  },
+  {
+    "revision": "5da434924670aa8bf02c",
+    "url": "/Attck-app/static/js/main.1cbd64a2.chunk.js"
+  },
+  {
+    "revision": "55e01c62b9f694502bbf",
+    "url": "/Attck-app/static/js/runtime~main.13d15cd9.js"
+  },
+  {
+    "revision": "78ea37bfc73bcb7b2e5b145a1472f5ec",
+    "url": "/Attck-app/static/media/starwars.78ea37bf.png"
+  }
+]);
